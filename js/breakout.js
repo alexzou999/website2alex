d65d455f60a24448ba23ee7035820fc25d459868
alexzou999 <142867203+alexzou999@users.noreply.github.com>
2024-03-19 10:18:35 -0700

    First commit

--- js/breakout.js
+++ js/breakout.js
@@ -1,6 +1,21 @@
 rulesBtn = document.getElementById('rules-btn')
 rules = document.getElementById('rules')
 closeBtn = document.getElementById('close-btn')
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
+
+//Create ball properties
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4;
+    dy: -4;
+}
+
+//Draw ball on canvas
+function drawBall()
 
 //Rules open and close event handlers
 rulesBtn.addEventListener('click', () => {
